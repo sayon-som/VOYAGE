a1baee81258a08b12eed62344ad918a59f971245
Sayon Som <sayonsom11b@gmail.com>
2022-04-06 01:26:20 +0530

    map updated

--- public/JS/map.js
+++ public/JS/map.js
@@ -1,14 +1,14 @@
-mapboxgl.accessToken =
-  map_token;
-const map = new mapboxgl.Ma({
-  container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
-});
-// Create a new marker.
+mapboxgl.accessToken =map_token;
+const loc = JSON.parse(new_data);
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: loc.geometry.coordinates, // starting position [lng, lat]
+zoom: 4 // starting zoom
+})
 // Set marker options.
-
-const marker = new mapboxgl.Marker()
-    .setLngLat([30.5, 50.5])
-    .addTo(map);
+// Set marker options.
+const marker = new mapboxgl.Marker({
+    color: "#FFFFFF",
+    draggable: true
+}).setLngLat(loc.geometry.coordinates).setPopup( new mapboxgl.Popup({offset:25}).setHTML(`<h2>${loc.title}</h2>`)).addTo(map);
--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 //getting the model
 
-const axios=require('axios');
+const axios = require("axios");
 const Campground = require("../models/CampGround");
 //connecting the database
-const cities=require("./cities");
-const {places,descriptors}=require('./seedhelpers');
+const cities = require("./cities");
+const { places, descriptors } = require("./seedhelpers");
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp")
   .then((data) => console.log("database connected successfully"))
@@ -15,45 +15,50 @@ mongoose
 //
 async function seedImg() {
   try {
-    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+    const resp = await axios.get("https://api.unsplash.com/photos/random", {
       params: {
-        client_id:'k4e7Y8jhecFbZONkWHKlO3yDKM-YLpYEwGZw0JeaIDc',
+        client_id: "k4e7Y8jhecFbZONkWHKlO3yDKM-YLpYEwGZw0JeaIDc",
         collections: 1114848,
       },
-    })
-    return resp.data.urls.small
+    });
+    return resp.data.urls.small;
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 //checking with a async function
-const rand_array=arr=>arr[Math.floor(Math.random()*arr.length)];
-const seedDB=async()=>{
-await Campground.deleteMany({});
-for(let i=0;i<50;i++){
-const rand_city=Math.floor((Math.random()*1000));
-const price=Math.floor(Math.random()*1000);
-const new_data = new Campground({
-  location: `${cities[rand_city].city} ${cities[rand_city].state}`,
-  title: `${rand_array(descriptors)} ${rand_array(places)}`,
-  price: price,
-  image: await seedImg(),
-  images: [
-    {
-      url: "https://res.cloudinary.com/dwvwzlueg/image/upload/v1646068462/VOYAGE/mpjfd7ujenogyhnlsjov.jpg",
-      filename: "VOYAGE/mpjfd7ujenogyhnlsjov",
-    },
-    {
-      url: "https://res.cloudinary.com/dwvwzlueg/image/upload/v1646068462/VOYAGE/i5c3mcijk3log20xoqf3.jpg",
-      filename: "VOYAGE/i5c3mcijk3log20xoqf3",
-    },
-  ],
-  description: "llorem ip asd dasdhasada dskjdkads dahdajdpidajf fiusfijfdf",
-  author: "620f4b4cb00c6007c75ecc64",
+const rand_array = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const rand_city = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 1000);
+    const new_data = new Campground({
+      location: `${cities[rand_city].city} ${cities[rand_city].state}`,
+      title: `${rand_array(descriptors)} ${rand_array(places)}`,
+      geometry: {
+        type: "Point",
+        coordinates: [cities[rand_city].longitude, cities[rand_city].latitude],
+      },
+      price: price,
+      image: await seedImg(),
+      images: [
+        {
+          url: "https://res.cloudinary.com/dwvwzlueg/image/upload/v1646068462/VOYAGE/mpjfd7ujenogyhnlsjov.jpg",
+          filename: "VOYAGE/mpjfd7ujenogyhnlsjov",
+        },
+        {
+          url: "https://res.cloudinary.com/dwvwzlueg/image/upload/v1646068462/VOYAGE/i5c3mcijk3log20xoqf3.jpg",
+          filename: "VOYAGE/i5c3mcijk3log20xoqf3",
+        },
+      ],
+      description:
+        "llorem ip asd dasdhasada dskjdkads dahdajdpidajf fiusfijfdf",
+      author: "620f4b4cb00c6007c75ecc64",
+    });
+    await new_data.save();
+  }
+};
+seedDB().then(() => {
+  mongoose.connection.close();
 });
-await new_data.save();
-
-
-}
-}
-seedDB().then(()=>{mongoose.connection.close()});
