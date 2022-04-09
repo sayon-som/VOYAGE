8b6caa89ee17b0ae48c3d34f38afd237c8256846
Sayon Som <sayonsom11b@gmail.com>
2022-04-09 22:36:58 +0530

    modified final app

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-if(process.env.NODE_ENV!=="production"){
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 // console.log(process.env.SAYON);
-const helmet=require('helmet');
+const helmet = require("helmet");
 //connection to the mongo uri
 const db_uri = process.env.MONGO_URI;
 //for mongoose injection
-const express_mongoose_sanitize=require('express-mongo-sanitize');
-const express=require('express')
+const express_mongoose_sanitize = require("express-mongo-sanitize");
+const express = require("express");
 const mongoose = require("mongoose");
 //"mongodb://localhost:27017/yelp-camp")
 const db_url = process.env.MONGO_URI || "mongodb://localhost:27017/yelp-camp";
@@ -18,46 +18,41 @@ mongoose
   .catch((e) => {
     Console.log("error");
   });
-const path=require("path")
-const app=express();
+const path = require("path");
+const app = express();
 //requiring the sessions
-const session=require('express-session');
+const session = require("express-session");
 
 //storing session in the mongoatlas
 
 const MongoStore = require("connect-mongo")(session);
 
-
 //requiring flash
-const flash=require('connect-flash');
-const sec=process.env.SECRET || "hello this is secret";
+const flash = require("connect-flash");
+const sec = process.env.SECRET || "hello this is secret";
 const store = new MongoStore({
   url: db_url,
   secret: sec,
   touchAfter: 24 * 60 * 60,
 });
-store.on('error',(e)=>{
+store.on("error", (e) => {
   console.log(e);
-})
+});
 const sessionConfig = {
   store,
   secret: sec,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    name:"lightyagami",
-    httpOnly:true,
+    name: "lightyagami",
+    httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
-const Usermodel=require("./models/User")
+const Usermodel = require("./models/User");
 const routes = require("./routes/CampgroundRouter");
 const rroutes = require("./routes/ReviewRoute");
-
-
-
-
 
 const review_model = require("./models/Review.js");
 //getting the error class
@@ -68,7 +63,7 @@ app.use(flash());
 
 //using the hemlet middleware
 // app.use(helmet()); //including this breaks the CSP
- 
+
 const scriptSrcUrls = [
   "https://stackpath.bootstrapcdn.com/",
   "https://api.tiles.mapbox.com/",
@@ -95,7 +90,7 @@ const connectSrcUrls = [
   "https://res.cloudinary.com/dwvwzlueg/",
 ];
 const fontSrcUrls = ["https://res.cloudinary.com/dwvwzlueg/"];
- 
+
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -123,41 +118,34 @@ app.use(
 
 //getting the model
 //for the data validation
-const joi=require('joi');
+const joi = require("joi");
 
-const joi_schema=require("./schema.js");
-const review_schema=require("./reviewschema.js");
+const joi_schema = require("./schema.js");
+const review_schema = require("./reviewschema.js");
 //using the ejs mate template
 
-const ejs=require('ejs-mate');
-const CatchAsync=require('./utils/CatchAsync');
-const Campground=require("./models/CampGround.js");
+const ejs = require("ejs-mate");
+const CatchAsync = require("./utils/CatchAsync");
+const Campground = require("./models/CampGround.js");
 //requiring passport
-const passport=require("passport");
-const localstrategy=require('passport-local');
-
-
-
-
-
+const passport = require("passport");
+const localstrategy = require("passport-local");
 
 const methodOverride = require("method-override"); //for the method override
-const { Console } = require('console');
-const { nextTick, ppid } = require('process');
-const res = require('express/lib/response');
-const { string } = require('joi');
-const Review = require('./models/Review.js');
+const { Console } = require("console");
+const { nextTick, ppid } = require("process");
+const res = require("express/lib/response");
+const { string } = require("joi");
+const Review = require("./models/Review.js");
 //getting the user route
-const UserRoute=require("./routes/User.js");
-const { id } = require('./schema.js');
+const UserRoute = require("./routes/User.js");
+const { id } = require("./schema.js");
 //for getting the body from the post request
 app.use(express.urlencoded({ extended: true }));
 
-
-app.engine('ejs',ejs);
+app.engine("ejs", ejs);
 app.set("view enjine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -171,24 +159,20 @@ passport.deserializeUser(Usermodel.deserializeUser());
 //using the mongo sanitize ckage
 app.use(express_mongoose_sanitize());
 
-
 //for handling the flash operations
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   //storing the return to url
-  
-  //creating the local for hiding and viewing functionalitites depending on the users choice
-  
-  res.locals.currentUser=req.user;
-  res.locals.success=req.flash('success');
-  res.locals.error=req.flash('error');
-  next();
-})
 
+  //creating the local for hiding and viewing functionalitites depending on the users choice
+
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //serving the public assets
-app.use(express.static(path.join(__dirname,'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //for the uuid
 app.use(methodOverride("__method"));
@@ -211,41 +195,30 @@ app.use(methodOverride("__method"));
 //   }
 // };
 
-
 //for the home route
-app.get("/",(req,res)=>{
-   res.render("home.ejs");
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
-
-
-
 
 //adding new user
 // app.get("/fakeuser",async(req,res)=>{
 //   const user= new Usermodel({
 //     email:"hello@gmial.com",
 //     username:"Sayon"
-    
+
 //   })
 //  const new_user=await  Usermodel.register(user,"123456");
 //  res.send(new_user);
 // })
 //for the user route
-app.use("/",UserRoute);
+app.use("/", UserRoute);
 
 //for the campground router part
-app.use("/",routes);
+app.use("/", routes);
 
 //for the review router part
 
-app.use("/",rroutes)
-
-
-
-
-
-
-
+app.use("/", rroutes);
 
 // app.get("/makecamp",async (req,res)=>{
 //     const new_camp= new Campground({title:"mexico",price:"$160",description:"good",location:"florida"});
@@ -265,9 +238,7 @@ app.use("/",rroutes)
 // res.render("campgrounds/index.ejs",{info})
 // })
 
-
 // //adding a new campground
-
 
 // app.get("/campgrounds/new",(req,res)=>{
 //   res.render("campgrounds/new.ejs");
@@ -277,13 +248,12 @@ app.use("/",rroutes)
 
 // app.post("/campgrounds",validator,CatchAsync(async(req,res)=>{
 //   //defining the joi schema
-  
-  
+
 //   const data=req.body;
 //   const new_camp= Campground({location:data.location,title:data.title});
 // await new_camp.save();
 // res.redirect("/campgrounds");
-// } 
+// }
 // )
 // )
 
@@ -319,7 +289,7 @@ app.use("/",rroutes)
 
 //editing and updating the form
 // app.get("/campgrounds/:id/edit",CatchAsync(async(req,res)=>{
-  
+
 //   const data=await Campground.findById(req.params.id);
 //   const {id}=req.params;
 //   res.render("campgrounds/edit.ejs",{id});
@@ -341,24 +311,23 @@ app.use("/",rroutes)
 // })
 // //showing all the campgrounds
 // app.get("/campgrounds/:id",async(req,res)=>{
-  
+
 //   const data = await Campground.findById(req.params.id).populate('reviews');
 //   //data contains all the reviews
 
 //   res.render("campgrounds/show.ejs",{data});
 // })
-app.all('*',(req,res,next)=>{
-  next(new ExpressError("Page Not Found",404
-  ));
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
-app.use((err,req,res,next)=>{
-  const {message="got a error buddy!!!!!!!",status=400}=err;
-  if(!err.message)err.message="something went wrong";
-  res.status(status).render('error.ejs',{err});
-})
+app.use((err, req, res, next) => {
+  const { message = "got a error buddy!!!!!!!", status = 400 } = err;
+  if (!err.message) err.message = "something went wrong";
+  res.status(status).render("error.ejs", { err });
+});
 //server response
-app.listen(8000,()=>{
-    console.log("listening at port 8000");
-})
-  
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT}`);
+});
